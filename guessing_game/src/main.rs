4962676641f8c3ab1eec706790a167de3aac5a34
Taylor Tang <taylor.t@tc-gaming.com>
2020-12-22 17:23:42 +0800

    2 improve guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,30 +1,37 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
 	println!("Guess the numer!");
+		let mut rng = rand::thread_rng();
+		let n1: u32 = rng.gen_range(0, 10);
 
-	println!("Please input your guess.(0~9)");
-	let mut rng = rand::thread_rng();
+	loop {
+		println!("Please input your guess.(0~9)");
+		let mut guess = String::new();
 
-	let n1: u8 = rng.gen_range(0, 10);
-	let mut guess = String::new();
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("Failed to read line");
 
-	io::stdin()
-		.read_line(&mut guess)
-		.expect("Failed to read line");
+//		println!("The number is: {}", n1);
+		println!("You guessed: {}", guess);
 
-	println!("The number is: {}", n1);
-	println!("You guessed: {}", guess);
+	//	guess.pop(); // remove linebreak
 
-	guess.pop(); // remove linebreak
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
 
-	let guess_num: u8 = guess.parse::<u8>().unwrap();
-
-	if n1 == guess_num {
-		println!("You win.");
-	} else {
-		println!("You lose.");
+		match guess.cmp(&n1) {
+			Ordering::Less => println!("low"),
+			Ordering::Greater => println!("high"),
+			Ordering::Equal => {
+				println!("hit, The number is: {}", n1);
+				break;
+			}
+		}
 	}
-
 }
